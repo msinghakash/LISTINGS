8d6fe8aa02f0848e8256e92557acfdbae0e3bdb0
akashsingh11596 <49496709+akashsingh11596@users.noreply.github.com>
2021-03-24 03:38:20 -0600

    Added Forgot Password UI

---
+++ app/src/main/java/Login_Signup/ForgotPassword_Options.java
@@ -0,0 +1,14 @@
+package Login_Signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.listings.R;
+public class ForgotPassword_Options extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password__options);
+    }
+}
---
+++ app/src/main/java/Login_Signup/SetNewPassword.java
@@ -0,0 +1,14 @@
+package Login_Signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetNewPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_new_password);
+    }
+}
