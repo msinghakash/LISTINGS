d8a8b97510872747d3cc5351d2801a8cdd26e16e
akashsingh11596 <49496709+akashsingh11596@users.noreply.github.com>
2021-03-31 14:09:00 -0600

    Validations for SignUp pages added. Firebase setup completed. Dependencies of firebase added. Dependency for authentication added.

--- app/src/main/java/Login_Signup/SignUp.java
+++ app/src/main/java/Login_Signup/SignUp.java
@@ -13,12 +13,15 @@ import android.widget.TextView;
 import android.widget.Button;
 
 import com.example.listings.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUp extends AppCompatActivity {
     //Variables
     ImageView backBtn;
     Button next;
     TextView titleText;
+    //Variables to get data from the signup screen
+    TextInputLayout fullname, email, username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +30,26 @@ public class SignUp extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
 
-        //Hooks
+        //Hooks for transition/animation
         backBtn = findViewById(R.id.signup_back_button);
         next = findViewById(R.id.signup_next_btn);
         titleText = findViewById(R.id.signup_title_text);
 
-
+        //Hooks for getting the data from signup page
+        fullname = findViewById(R.id.enter_full_name);
+        email = findViewById(R.id.enter_email);
+        username = findViewById(R.id.enter_username);
+        password = findViewById(R.id.enter_password);
     }
 
     public void callNextSignupPage(View view) {
 
-        Intent intent = new Intent(SignUp.this, SignUp2ndPageClass.class);
+        if(!validateFullName() | !validateEmail() | !validateUsername() | !validatePassword())
+        {
+            return;
+        }
+
+        Intent intent = new Intent(getApplicationContext(), SignUp2ndPageClass.class);
         //Adding Transition
         // 3 below represents the total number of transitions we want to perform
         Pair[] pairs = new Pair[3];
@@ -49,5 +61,87 @@ public class SignUp extends AppCompatActivity {
         // ActivityOptions to transition between Activities using cross-Activity scene animations.
         ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp.this, pairs);
         startActivity(intent, options.toBundle()); //options.toBundle() will attach animations to the intent.
+    }
+
+    // Functions for validations
+    //Function for getting data
+    private Boolean validateFullName() {
+        //trim to remove spaces
+        String fullname_value = fullname.getEditText().getText().toString().trim();
+        if (fullname_value.isEmpty()) {
+            fullname.setError("Please enter name!");
+            return false;
+        } else {
+            //This portion of the code will be executed when the full name is not empty
+            //This is going to be the true statement case
+            fullname.setError(null);//This will remove the error
+            fullname.setErrorEnabled(false);//This will remove the space between each field that is taken to display error.
+            return true;
+        }
+    }
+
+    private Boolean validateEmail() {
+        //trim to remove spaces
+        String email_value = email.getEditText().getText().toString().trim();
+        String validate_email =  "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (email_value.isEmpty()) {
+            email.setError("Please enter name!");
+            return false;
+        } else if (email_value.contains(" ")) {
+            email.setError("Please enter without space!");
+            return false;
+        } else if (email_value.matches(validate_email)) {
+            email.setError("Username cannot be more than 30 letters!");
+            return false;
+        } else {
+            //This portion of the code will be executed when the full name is not empty
+            //This is going to be the true statement case
+            email.setError(null);//This will remove the error
+            email.setErrorEnabled(false);//This will remove the space between each field that is taken to display error.
+            return true;
+        }
+    }
+
+    private Boolean validateUsername() {
+        //trim to remove spaces
+        String username_value = username.getEditText().getText().toString().trim();
+
+        if (username_value.isEmpty()) {
+            username.setError("Please enter name!");
+            return false;
+        } else if (username_value.contains(" ")) {
+            username.setError("Please enter without space!");
+            return false;
+        } else {
+            //This portion of the code will be executed when the full name is not empty
+            //This is going to be the true statement case
+            username.setError(null);// This will remove the error
+            username.setErrorEnabled(false);//This will remove the space between each field that is taken to display error.
+            return true;
+        }
+    }
+
+    private Boolean validatePassword() {
+        //trim to remove spaces
+        String password_value = password.getEditText().getText().toString().trim();
+        String validate_password = "^(?=.*[A-Za-z])(?=.*\\\\d)(?=.*[$@$!%*#?&])[A-Za-z\\\\d$@$!%*#?&]{8,}$";
+
+        if (password_value.isEmpty()) {
+            password.setError("Please enter name!");
+            return false;
+        } else if (password_value.contains(" ")) {
+            password.setError("Please enter without space!");
+            return false;
+        } else if (password_value.matches(validate_password)) {
+            password.setError("Username cannot be more than 30 letters!");
+            return false;
+        } else {
+            //This portion of the code will be executed when the full name is not empty
+            //This is going to be the true statement case
+            password.setError(null);//This will remove the error
+            password.setErrorEnabled(false);//This will remove the space between each field that is taken to display error.
+            return true;
+        }
     }
 }
--- app/src/main/java/Login_Signup/SignUp2ndPageClass.java
+++ app/src/main/java/Login_Signup/SignUp2ndPageClass.java
@@ -8,17 +8,25 @@ import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.listings.R;
+
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SignUp2ndPageClass extends AppCompatActivity {
     //Variables
     ImageView backBtn;
     Button next;
     TextView titleText;
+    RadioGroup radioGroup;
+    RadioButton choosenGender;
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +39,27 @@ public class SignUp2ndPageClass extends AppCompatActivity {
         backBtn = findViewById(R.id.signup_back_button);
         next = findViewById(R.id.signup_next_btn);
         titleText = findViewById(R.id.signup_title_text);
+        radioGroup = findViewById(R.id.radiogroup);
+        datePicker = findViewById(R.id.select_age);
 
     }
 
     public void callNextSignupPage(View view) {
+        if(!validateGender())
+        {
+            return;
+        }
+
+        choosenGender = findViewById(radioGroup.getCheckedRadioButtonId());
+        String _gender = choosenGender.getText().toString();// '_' is at the start of the String variable because it is a local String
+
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+        //Combining all variables to form date
+        String date = day+"/"+month+"/"+year;
+
+
         Intent intent = new Intent(getApplicationContext(), SignUp3rdPageClass.class);
         //Adding Transition
         // 3 below represents the total number of transitions we want to perform
@@ -47,5 +72,20 @@ public class SignUp2ndPageClass extends AppCompatActivity {
         // ActivityOptions to transition between Activities using cross-Activity scene animations.
         ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(SignUp2ndPageClass.this, pairs);
         startActivity(intent, options.toBundle()); //options.toBundle() will attach animations to the intent.
+    }
+
+    private boolean validateGender() {
+        if (radioGroup.getCheckedRadioButtonId() == -1)//-1 represents that no radiobutton is selected.
+        {
+            Toast.makeText(this, "Please select gender", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateAge()
+    {
+        int userAge = datePicker.getYear();
     }
 }
